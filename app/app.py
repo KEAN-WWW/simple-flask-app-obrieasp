c05a7cd240401c9730782db53651c414ab6487ce
obrieasp <obrieasp@kean.edu>
2025-04-08 10:12:15 -0400

    submit 9

--- app/app.py
+++ app/app.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+valid_users = ["jack", "jill", "alice"]
+
 @app.route("/")
 def home():
-    return "<h1>Hello CPS3500!</h1>"
-
-@app.route("/new_page")
-def new_page():
-    return "<h1>This is a New Page!</h1>"
+    return "Welcome to the homepage"
 
 @app.route("/user/<username>")
-def user(username):
-    return render_template("user.html", username=username)
+def greet_user(username):
+    if username.lower() == "none":
+        return "No user provided", 400
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    if username.lower() in valid_users:
+        return render_template("user.html", username=username)
+    else:
+        return "Invalid user", 403
+
+@app.route("/new")
+def new_page():
+    return render_template("user.html", new_page=True)
